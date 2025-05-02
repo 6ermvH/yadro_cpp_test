7f185d695886144d59b29ccc17e407a127fe799a
German Feskov <g.feskov@yandex.ru>
2025-05-02 08:48:41 +0300

    add: user manager

---
+++ include/manager_user.hpp
@@ -0,0 +1,18 @@
+#ifndef MANAGER_USER_HPP
+#define MANAGER_USER_HPP
+
+class ManagerUser {
+public:
+    ManagerUser() = default;
+    ~ManagerUser() = default;
+
+    void add(const std::string& username, user_id_t user_id);
+    void wait(const std::string& username);
+    void remove(const std::string& username);
+
+private:
+    std::unordered_map<std::string, user_id_t> user_map_;
+    std::list<user_id_t> waiting_group_; 
+};
+
+#endif // !MANAGER_USER_HPP
