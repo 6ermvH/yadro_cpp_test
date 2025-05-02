fa22cf3ec876f81109219bee8d90efefb72eb266
German Feskov <g.feskov@yandex.ru>
2025-05-02 21:08:44 +0300

    add: test manager pc base

---
+++ tests/manager_pc_test.cc
@@ -0,0 +1,10 @@
+#include "manager_pc.hpp"
+
+TEST(ManagerPCTest, AssignPC) {
+    ManagerPC manager(5);
+    Time start_time(9, 0);
+    user_id_t user_id = 1;
+
+    manager.assign(0, user_id, start_time);
+    EXPECT_EQ(manager.get_free_pc(), 1);
+}
